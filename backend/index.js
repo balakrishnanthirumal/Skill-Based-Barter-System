50e3f34fb3cf5ad0bebcd95efdf956f39f8f9da1
balakrishnanthirumal <balakrishnanthirumalt@gmail.com>
2025-07-21 08:21:49 +0530

    User Authentication

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/lib/db.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -8,6 +10,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectDB();
